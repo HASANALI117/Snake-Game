c2cb3b682735e2b468be99b28fdd7931d723ce30
hasanali117 <hassanali61911@gmail.com>
2023-05-21 21:34:35 +0300

    snake now moves in all directions on key press and stops when hitting the border of the gamebox

--- main.js
+++ main.js
@@ -7,26 +7,60 @@ let snakey;
 let snakeDivs = {
   x: 5,
   y: 5,
-  // { x: 6, y: 6 },
 };
-// snake.css("grid-row", snakeDivs.y);
-// snake.css("grid-column", snakeDivs.x);
+
+let currentX = Number(snake.css("grid-column").split("/")[0]);
+let currentY = Number(snake.css("grid-row").split("/")[0]);
 
 let foodx = Math.floor(Math.random() * 10) + 1;
 let foody = Math.floor(Math.random() * 10) + 1;
+
+let direction = {
+  dx: 1,
+  dy: 0,
+};
+const dir = {
+  d: {
+    dx: 1,
+    dy: 0,
+  },
+  w: {
+    dx: 0,
+    dy: -1,
+  },
+  a: {
+    dx: -1,
+    dy: 0,
+  },
+  s: {
+    dx: 0,
+    dy: 1,
+  },
+};
 
 function randomFood() {
   food.css("grid-area", `${foody} / ${foodx}`);
 }
 randomFood();
 
-let i = 1;
+let x = 1;
+let y = 1;
+let myInterval = setInterval(move, 150);
 function move() {
-  let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  if (i <= 10) {
-    snake.css("grid-column", i);
-    console.log(arr[i]);
+  // let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  if (x <= 10 && y <= 10 && x >= 1 && y >= 1) {
+    snake.css("grid-area", `${y} / ${x}`);
+    console.log("x value: " + currentX + "     y value: " + currentY);
+    // console.log(arr[x]);
+    x += direction.dx;
+    y += direction.dy;
+  } else {
+    clearInterval(myInterval);
+    alert("Game Over");
   }
-  i++;
 }
-setInterval(move, 650);
+// console.log(direction["d"]);
+$("body").on("keydown", function (e) {
+  console.log(e.key);
+  direction = dir[e.key];
+});
